7a931559320c4b26a666aaa8e12d4912c7a499fb
Namnpse <nam444479@gmail.com>
2024-01-23 00:38:55 +0900

    done handle dark theme in login screen

--- app/src/main/java/com/namnp/instagram_android/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/namnp/instagram_android/presentation/login/LoginScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -32,6 +33,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -45,13 +47,13 @@ import com.namnp.instagram_android.presentation.MainViewModel
 import com.namnp.instagram_android.presentation.login.composables.InputAccount
 import com.namnp.instagram_android.presentation.login.composables.SignupSuggestion
 import com.namnp.instagram_android.presentation.ui.spacing
-import com.namnp.instagram_android.presentation.ui.theme.Black400
-import com.namnp.instagram_android.presentation.ui.theme.Black500
 import com.namnp.instagram_android.presentation.ui.theme.Blue200
-import com.namnp.instagram_android.presentation.ui.theme.Gray200
+import com.namnp.instagram_android.presentation.ui.theme.Grey200
 import com.namnp.instagram_android.presentation.ui.theme.InstagramComposeAndroidTheme
+import com.namnp.instagram_android.presentation.ui.theme.backIcon
 import com.namnp.instagram_android.presentation.ui.theme.text600_14
 import com.namnp.instagram_android.presentation.ui.theme.textNormal_12
+import com.namnp.instagram_android.presentation.ui.theme.textSecondary
 import com.namnp.instagram_android.utils.Theme
 
 @Composable
@@ -62,11 +64,14 @@ fun LoginScreen(
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     val themeState by mainViewModel.themeState.collectAsState()
-
+    var loginEnabled by remember { mutableStateOf(false) }
+    LaunchedEffect(key1 = username, key2 = password) {
+        loginEnabled = username.isNotEmpty() && password.isNotEmpty()
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.White),
+            .background(color = MaterialTheme.colorScheme.background),
         verticalArrangement = Arrangement.SpaceBetween,
     ) {
         Icon(
@@ -77,7 +82,7 @@ fun LoginScreen(
                 },
             imageVector = Icons.Filled.ArrowBackIos,
             contentDescription = null,
-            tint = Black500
+            tint = MaterialTheme.colorScheme.backIcon
         )
         Column(
             modifier = Modifier.fillMaxWidth(),
@@ -88,7 +93,6 @@ fun LoginScreen(
                 checked = themeState.theme == Theme.Dark,
                 colors = SwitchDefaults.colors(
                     checkedThumbColor = Blue200,
-//                    checkedTrackColor = Color.ShimmerLightGray
                 ),
                 onCheckedChange = { _ ->
                     mainViewModel.toggleDarkMode()
@@ -96,7 +100,8 @@ fun LoginScreen(
             )
             Image(
                 painter = painterResource(id = R.drawable.ic_instagram_logo),
-                contentDescription = null
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.backIcon)
             )
             Spacer(modifier = Modifier.height(40.dp))
             InputAccount(
@@ -134,13 +139,16 @@ fun LoginScreen(
                     containerColor = Blue200,
                     disabledContainerColor = Blue200.copy(alpha = 0.5f)
                 ),
-                enabled = username.isNotEmpty() && password.isNotEmpty(),
+                enabled = loginEnabled,
                 onClick = {}
             ) {
                 Text(
                     modifier = Modifier.padding(6.dp),
                     text = stringResource(id = R.string.log_in),
-                    style = text600_14
+                    style = text600_14.copy(
+                        color = if (loginEnabled) Color.White
+                        else Color.White.copy(alpha = 0.5f),
+                    )
                 )
             }
             Spacer(modifier = Modifier.height(30.dp))
@@ -169,17 +177,19 @@ fun LoginScreen(
             ) {
                 Divider(
                     modifier = Modifier.weight(1f),
-                    color = Gray200,
+                    color = Grey200,
                     thickness = 1.dp,
                 )
                 Text(
                     modifier = Modifier.padding(horizontal = 30.dp, vertical = 30.dp),
                     text = stringResource(id = R.string.or).uppercase(),
-                    style = text600_14.copy(color = Black400)
+                    style = text600_14.copy(
+                        color = MaterialTheme.colorScheme.textSecondary
+                    )
                 )
                 Divider(
                     modifier = Modifier.weight(1f),
-                    color = Gray200,
+                    color = Grey200,
                     thickness = 1.dp,
                 )
             }
@@ -190,14 +200,17 @@ fun LoginScreen(
             )
         }
         Column {
-            Divider(color = Gray200, thickness = 1.dp)
+            Divider(color = Grey200, thickness = 1.dp)
             Text(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(18.dp),
                 text = stringResource(id = R.string.instagram_vs_facebook),
                 style = textNormal_12
-                    .copy(color = Black400, fontWeight = FontWeight(400))
+                    .copy(
+                        color = MaterialTheme.colorScheme.textSecondary,
+                        fontWeight = FontWeight(400),
+                    )
             )
         }
     }
